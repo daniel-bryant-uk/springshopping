2322d9594788595432b787430589ffd46a1335f6
Daniel Bryant <daniel.bryant@tai-dev.co.uk>
2016-03-25 17:31:05 +0000

    Add actuator and tidy code

--- stockmanager/src/main/java/uk/co/taidev/springshopping/controllers/StockController.java
+++ stockmanager/src/main/java/uk/co/taidev/springshopping/controllers/StockController.java
@@ -2,12 +2,9 @@ package uk.co.taidev.springshopping.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import uk.co.taidev.springshopping.exceptions.StockNotFoundException;
 import uk.co.taidev.springshopping.model.Stock;
-import uk.co.taidev.springshopping.services.ProductService;
 import uk.co.taidev.springshopping.services.StockService;
 
 @RestController
